654ba35d70d9ced5c33dc5809889d6de5ef425af
LoonyLadle <loonyladel@gmail.com>
2019-03-06 19:14:43 -0500

    2.0.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hediff Apparel Resource
+Adds a ThingComp usable in apparel defs to inflict hediffs on the wearer, without any C# required.
---
+++ Source/HediffApparel/FilterMode.cs
@@ -0,0 +1,9 @@
+﻿#pragma warning disable IDE1006 // Naming Styles
+
+namespace HediffApparel
+{
+    public enum FilterMode : byte
+    {
+        Contains, StartsWith, EndsWith, Equals, Excludes
+    }
+}
